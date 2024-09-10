d648a6d29651c4278cece6122c490f262bbbdb7e
Pablo Boo <pabloboocastrofeito@gmail.com>
2024-09-10 17:31:50 +0200

    Designed slides of the web

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let currentPage = 1;
-const totalPages = 3;
+const totalPages = 5;
 
 function scrollNext() {
     if (currentPage < totalPages) {
@@ -13,6 +13,11 @@ function scrollPrev() {
         currentPage--;
         document.getElementById('page' + currentPage).scrollIntoView({ behavior: 'smooth' });
     }
+}
+
+function goToFirstPage() {
+    currentPage = 1;
+    document.getElementById('page1').scrollIntoView({ behavior: 'smooth' });
 }
 
 window.addEventListener('wheel', (event) => {
